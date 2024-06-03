47e3c74bf540498dae3df954364107cf962cab98
umar006 <umaralfaruq597@gmail.com>
2024-06-03 20:53:09 +0700

    feat: sort import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import databaseConfig from './database/database.config';
 import { DatabaseModule } from './database/database.module';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [

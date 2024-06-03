54022be7cf7efd53330ac0687d43cd9522f937c1
umar006 <umaralfaruq597@gmail.com>
2024-06-03 13:34:14 +0700

    feat: setup drizzle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import databaseConfig from './database/database.config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       load: [databaseConfig],
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],

1d2531e3608623b99cffae278d54233dad6fda68
umar006 <umaralfaruq597@gmail.com>
2024-06-03 20:13:55 +0700

    feat: setup rabbitmq producer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import databaseConfig from './database/database.config';
 import { DatabaseModule } from './database/database.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -11,6 +12,19 @@ import { DatabaseModule } from './database/database.module';
       isGlobal: true,
       load: [databaseConfig],
     }),
+    ClientsModule.register([
+      {
+        name: 'TEST_PRODUCER',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'test-queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
     DatabaseModule,
   ],
   controllers: [AppController],

09efea8b7330af93c409c76ede440c1fb299e8b9
umar006 <umaralfaruq597@gmail.com>
2024-06-03 13:57:36 +0700

    feat: test01 schema and migration file

---
+++ src/app.schema.ts
@@ -0,0 +1,15 @@
+import {
+  datetime,
+  int,
+  mysqlTable,
+  smallint,
+  varchar,
+} from 'drizzle-orm/mysql-core';
+
+export const test01 = mysqlTable('Test01', {
+  id: int('id').autoincrement().primaryKey(),
+  nama: varchar('name', { length: 100 }),
+  status: smallint('status'),
+  created: datetime('created'),
+  updated: datetime('updated'),
+});

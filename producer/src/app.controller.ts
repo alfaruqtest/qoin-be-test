71324773a2fbd7de90d63cd11de7aae97a0cb8e0
umar006 <umaralfaruq597@gmail.com>
2024-06-03 22:20:52 +0700

    delete: unncessary producer code

--- producer/src/app.controller.ts
+++ producer/src/app.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  Query,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
 import { CreateTestDTO, UpdateTestDTO } from './app.dto';
 import { AppService } from './app.service';
 
@@ -21,21 +12,6 @@ export class AppController {
     return { message: resp };
   }
 
-  @Get('test/:id')
-  async getTestByID(@Param('id') id: number) {
-    const resp = await this.appService.getTestByID(id);
-    return { data: resp };
-  }
-
-  @Get('test')
-  async getTestList(
-    @Query('limit') limit: number,
-    @Query('offset') offset: number,
-  ) {
-    const resp = await this.appService.getTestList(limit, offset);
-    return { data: resp };
-  }
-
   @Put('test/:id')
   updateTestByID(@Param('id') id: number, @Body() body: UpdateTestDTO) {
     const resp = this.appService.updateTestByID(id, body);
@@ -46,10 +22,5 @@ export class AppController {
   deleteTestByID(@Param('id') id: number) {
     const resp = this.appService.deleteTestByID(id);
     return { message: resp };
-  }
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
   }
 }
--- producer/src/app.module.ts
+++ producer/src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import databaseConfig from './database/database.config';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [databaseConfig],
-    }),
     ClientsModule.register([
       {
         name: 'TEST_PRODUCER',
@@ -25,7 +18,6 @@ import { DatabaseModule } from './database/database.module';
         },
       },
     ]),
-    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
